524b397958e25e228a012e24eabf212c946b57f9
ilyes12zouaoui <ilyes.zouaoui@esprit.tn>
2019-07-22 19:07:30 +0200

    adding link to react-rdux documentation in Home component

--- src/router/PageNotFound.js
+++ src/router/PageNotFound.js
@@ -20,11 +20,17 @@ export default () => {
           librairy which help us to create redux store, action creator and
           reducers in a much simpler way then we used to do before using redux
           library directly, also it includes redux-thunk middleware by default
-          for handling asynchronous operations, and we use react-redux library
-          to be able to connect our react app with our redux solution in a
-          simple way, this project was made for learning how to manage our react
-          application state in a more efficient way using redux, i wish it helps
-          you.
+          for handling asynchronous operations, and we use{" "}
+          <a
+            href="https://react-redux.js.org/introduction/quick-start"
+            target="_blank"
+          >
+            react-redux
+          </a>{" "}
+          library to be able to connect our react app with our redux solution in
+          a simple way, this project was made for learning how to manage our
+          react application state in a more efficient way using redux, i wish it
+          helps you.
         </p>
       </div>
     </div>
